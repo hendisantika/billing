d8e551c90561510d2876902d42b1c8d12ebc001b
hendisantika <hendisantika@yahoo.co.id>
2024-05-14 09:53:52 +0700

    Add EmailStrategy🫸🌀✏️📗🐧🐳

---
+++ notifier/src/main/java/id/my/hendisantika/billing/notifier/strategy/impl/EmailStrategy.java
@@ -0,0 +1,27 @@
+package id.my.hendisantika.billing.notifier.strategy.impl;
+
+import id.my.hendisantika.billing.notifier.strategy.NotificationStrategy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : billing
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/14/24
+ * Time: 09:53
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+public class EmailStrategy implements NotificationStrategy {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(NotificationProcessorServiceImpl.class);
+
+    private EmailNotificationService emailNotificationService;
+    private Template template;
+    private Configuration freeMarkerConfiguration;
+
+}
