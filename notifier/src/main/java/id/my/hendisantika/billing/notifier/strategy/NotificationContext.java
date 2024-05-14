8a3964ff3d4af4fe0258774bfa5b08942251709c
hendisantika <hendisantika@yahoo.co.id>
2024-05-14 09:52:39 +0700

    Add Bean in NotificationContext🫸🌀✏️📗🐧🐳

--- notifier/src/main/java/id/my/hendisantika/billing/notifier/strategy/NotificationContext.java
+++ notifier/src/main/java/id/my/hendisantika/billing/notifier/strategy/NotificationContext.java
@@ -3,6 +3,7 @@ package id.my.hendisantika.billing.notifier.strategy;
 import id.my.hendisantika.billing.notifier.model.Billing;
 import id.my.hendisantika.billing.notifier.model.Customer;
 import id.my.hendisantika.billing.notifier.model.NotificationType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -27,5 +28,15 @@ public class NotificationContext {
     public void generate(NotificationType type, Billing billing, Customer customer) throws Exception {
         strategies.values().stream().filter(strategy -> strategy.match(type)).collect(Collectors.toList()).get(0)
                 .generate(billing, customer);
+    }
+
+    @Autowired
+    public void setSmsStrategy(SmsStrategy smsStrategy) {
+        strategies.put(NotificationType.SMS, smsStrategy);
+    }
+
+    @Autowired
+    public void setEmailStrategy(EmailStrategy emailStrategy) {
+        strategies.put(NotificationType.EMAIL, emailStrategy);
     }
 }
