929ba701634c9e003581c76407c54248725bce84
hendisantika <hendisantika@yahoo.co.id>
2024-05-14 10:13:12 +0700

    Add findByDueDate in BillingService 🫸🌀✏️📗🐧🐳

--- notifier/src/main/java/id/my/hendisantika/billing/notifier/service/BillingService.java
+++ notifier/src/main/java/id/my/hendisantika/billing/notifier/service/BillingService.java
@@ -1,9 +1,13 @@
 package id.my.hendisantika.billing.notifier.service;
 
 import id.my.hendisantika.billing.notifier.dao.BillingDao;
+import id.my.hendisantika.billing.notifier.model.Billing;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +24,8 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class BillingService {
     private final BillingDao billingDao;
+
+    public List<Billing> findByDueDate(Instant instant) {
+        return billingDao.findByDueDate(instant);
+    }
 }
