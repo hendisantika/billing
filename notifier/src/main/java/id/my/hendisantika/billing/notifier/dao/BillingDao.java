dbe7629e08af5fc11ba2017bcc4624ae6317c1e8
hendisantika <hendisantika@yahoo.co.id>
2024-05-14 10:07:51 +0700

    Add findByDueDate in BillingDao🫸🌀✏️📗🐧🐳

--- notifier/src/main/java/id/my/hendisantika/billing/notifier/dao/BillingDao.java
+++ notifier/src/main/java/id/my/hendisantika/billing/notifier/dao/BillingDao.java
@@ -1,9 +1,14 @@
 package id.my.hendisantika.billing.notifier.dao;
 
+import id.my.hendisantika.billing.notifier.model.Billing;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.time.Instant;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +25,10 @@ import org.springframework.stereotype.Repository;
 public class BillingDao {
     @PersistenceContext
     private final EntityManager entityManager;
+
+    public List<Billing> findByDueDate(Instant instant) {
+        Query query = entityManager.createQuery("from Billing where dueDate <= :instant and billingNotificationStatus='NEW'");
+        query.setParameter("instant", instant);
+        return query.getResultList();
+    }
 }
