517455a3c77ed85bd5d818532c9635d9caa20034
hendisantika <hendisantika@yahoo.co.id>
2024-05-14 09:56:44 +0700

    Add generate in SmsStrategy🫸🌀✏️📗🐧🐳

--- notifier/src/main/java/id/my/hendisantika/billing/notifier/strategy/impl/SmsStrategy.java
+++ notifier/src/main/java/id/my/hendisantika/billing/notifier/strategy/impl/SmsStrategy.java
@@ -2,10 +2,19 @@ package id.my.hendisantika.billing.notifier.strategy.impl;
 
 import freemarker.template.Configuration;
 import freemarker.template.Template;
+import id.my.hendisantika.billing.notifier.model.Billing;
+import id.my.hendisantika.billing.notifier.model.Customer;
+import id.my.hendisantika.billing.notifier.model.NotificationStatus;
 import id.my.hendisantika.billing.notifier.model.NotificationType;
+import id.my.hendisantika.billing.notifier.model.SmsNotification;
 import id.my.hendisantika.billing.notifier.strategy.NotificationStrategy;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,5 +36,21 @@ public class SmsStrategy implements NotificationStrategy {
     @Override
     public boolean match(NotificationType type) {
         return type == NotificationType.SMS;
+    }
+
+    @Override
+    public void generate(Billing billing, Customer customer) throws Exception {
+        Map<String, Object> model = new HashMap<>();
+        model.put("billing", billing);
+        model.put("customer", customer);
+
+        String content = FreeMarkerTemplateUtils.processTemplateIntoString(template, model);
+
+        SmsNotification smsNotification = new SmsNotification();
+        smsNotification.setContent(content);
+        smsNotification.setMobile(customer.getMobile());
+        smsNotification.setStatus(NotificationStatus.NEW);
+        smsNotification.setCreatedDate(Instant.now());
+        smsNotificationService.add(smsNotification);
     }
 }
