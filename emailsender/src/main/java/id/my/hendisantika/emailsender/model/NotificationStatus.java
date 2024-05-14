bca5f9cbb97c1920931429ac561833673098685e
hendisantika <hendisantika@yahoo.co.id>
2024-05-14 09:12:52 +0700

    Add NotificationStatus Model 🫸🌀✏️📗🐧🐳

---
+++ emailsender/src/main/java/id/my/hendisantika/emailsender/model/NotificationStatus.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.emailsender.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : billing
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/14/24
+ * Time: 09:12
+ * To change this template use File | Settings | File Templates.
+ */
+public enum NotificationStatus {
+    NEW,
+    SENT,
+    FAILED
+}
