35d22d90f8f5a5c826be33a3f8059e26f63e566d
hendisantika <hendisantika@yahoo.co.id>
2024-05-14 09:17:32 +0700

    Add EmailNotificationService 🫸🌀✏️📗🐧🐳

---
+++ emailsender/src/main/java/id/my/hendisantika/emailsender/service/EmailNotificationService.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.emailsender.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : billing
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/14/24
+ * Time: 09:16
+ * To change this template use File | Settings | File Templates.
+ */
+@Transactional
+@Service
+@RequiredArgsConstructor
+public class EmailNotificationService {
+    private final EmailNotificationRepository emailNotificationRepository;
+}
