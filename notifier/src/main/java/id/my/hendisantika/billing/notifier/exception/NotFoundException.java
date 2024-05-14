2f95ae151b91008da4694860a96a2e0c7647dcf7
hendisantika <hendisantika@yahoo.co.id>
2024-05-14 10:11:36 +0700

    Add NotFoundException 🫸🌀✏️📗🐧🐳

---
+++ notifier/src/main/java/id/my/hendisantika/billing/notifier/exception/NotFoundException.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.billing.notifier.exception;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : billing
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/14/24
+ * Time: 10:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
