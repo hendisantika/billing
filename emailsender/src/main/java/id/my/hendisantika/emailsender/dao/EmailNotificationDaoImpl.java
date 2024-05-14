adeda2b29e2bd2f7df89f9f74acfb7ea9a219724
hendisantika <hendisantika@yahoo.co.id>
2024-05-14 09:19:16 +0700

    Add find in EmailNotificationDaoImpl 🫸🌀✏️📗🐧🐳

--- emailsender/src/main/java/id/my/hendisantika/emailsender/dao/EmailNotificationDaoImpl.java
+++ emailsender/src/main/java/id/my/hendisantika/emailsender/dao/EmailNotificationDaoImpl.java
@@ -1,8 +1,12 @@
 package id.my.hendisantika.emailsender.dao;
 
+import id.my.hendisantika.emailsender.model.EmailNotification;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +24,9 @@ public class EmailNotificationDaoImpl implements EmailNotificationRepository {
     @PersistenceContext
     private EntityManager entityManager;
 
+    @Override
+    public List<EmailNotification> find() {
+        Query query = entityManager.createQuery("from EmailNotification e where e.status='NEW'");
+        return query.getResultList();
+    }
 }
